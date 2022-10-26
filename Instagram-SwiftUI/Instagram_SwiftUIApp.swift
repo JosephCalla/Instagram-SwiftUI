0244bd757f0a7175a5facbe1c85e151684bb1267
Joseph Calla <josephestanislao9@gmail.com>
2022-10-26 10:59:08 -0500

    Initial Commit

---
+++ Instagram-SwiftUI/Instagram_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_SwiftUIApp.swift
+//  Instagram-SwiftUI
+//
+//  Created by Joseph Estanislao Calla Moreyra on 26/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
