aee1c72ad7bbaa1114ce0c384117ba848bb9ce37
Joseph Calla <35270796+JosephCalla@users.noreply.github.com>
2022-11-09 08:33:37 -0500

    NOTICKET: Update assets

--- Instagram-SwiftUI/Views/ProfileView.swift
+++ Instagram-SwiftUI/Views/ProfileView.swift
@@ -9,7 +9,79 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                ProfileHeaderView()
+                Spacer()
+            }
+            .navigationTitle("Joseph Calla")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+struct ProfileHeaderView: View {
+    var body: some View {
+        VStack {
+            // avatar image, counts
+            HStack {
+                Image("person2")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 90, height: 90, alignment: .center)
+                    .cornerRadius(45)
+                VStack {
+                    HeaderCountButtonsView()
+                    
+                    // Edit Profile button
+                    Button {
+                        
+                    } label: {
+                        Text("Edit Profile")
+                            .foregroundColor(Color(.label))
+                            .frame(width: 220, height: 44, alignment: .center)
+                            .background(Color(.secondarySystemBackground))
+                            .cornerRadius(8)
+                    }
+                    
+                    // user bio
+                    
+                }
+            }
+        }
+    }
+}
+
+struct HeaderCountButtonsView: View {
+    var body: some View {
+        HStack {
+            Button {
+                // no operation
+            } label: {
+                Text("200\nPoats")
+                    .foregroundColor(Color(.label))
+                    .multilineTextAlignment(.center)
+            }
+            .padding()
+            
+            Button {
+                // no operation
+            } label: {
+                Text("223k\nFollowers")
+                    .foregroundColor(Color(.label))
+                    .multilineTextAlignment(.center)
+            }
+            .padding()
+            
+            Button {
+                // no operation
+            } label: {
+                Text("100\nFollowing")
+                    .foregroundColor(Color(.label))
+                    .multilineTextAlignment(.center)
+            }
+            .padding()
+        }
     }
 }
 
